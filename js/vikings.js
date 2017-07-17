bd76f0945641cd7fffb314986384dc4cccdb3602
koshhi <cesaralvarezmarin@gmail.com>
2017-07-17 16:15:34 +0200

    vikings population contructor

---
+++ js/vikings.js
@@ -0,0 +1,9 @@
+var Population = function (name, health, strength){
+	this.name= name;
+	this.health= health;
+	this.strength=strength;
+};
+
+viking1= new Population ("Ragnarok", 1000, 150);
+viking2= new Population ("Asgard", 1500, 250);
+viking3= new Population ("Fernandito", 2000, 100);

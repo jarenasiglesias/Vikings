e36712a233276e844e355162416f51bea79ba58b
koshhi <cesaralvarezmarin@gmail.com>
2017-07-17 18:13:52 +0200

    created saxonGenerator

--- js/vikings.js
+++ js/vikings.js
@@ -1,3 +1,6 @@
+//viking pitFight
+
+
 var Population = function (name, health, strength){
 	this.health = health;
 	this.strength =strength;
@@ -17,40 +20,44 @@ vikingArray = [viking1, viking2, viking3];
 function pitFight(){
 
 	var roundNumber = getRandomInt(5,8);
-	var vikingFighter1 = vikingArray [getRandomInt(0,2)];
-  	var vikingFighter2 = vikingArray [getRandomInt(0,2)];
-  	var vikingFighter1Life = vikingFighter1.health;
-  	var vikingFighter2Life = vikingFighter2.health;
+  var vikingFighter1 = vikingArray [getRandomInt(0,2)];
+  var vikingFighter2 = vikingArray [getRandomInt(0,2)];
+  var vikingFighter1Life = vikingFighter1.health;
+  var vikingFighter2Life = vikingFighter2.health;
 	for (var i=0; i<=roundNumber; i++){
-	    vikingFighter1Life = vikingFighter1Life - vikingFighter2.strength;
-	    vikingFighter2Life = vikingFighter2Life - vikingFighter1.strength;
-    	if (vikingFighter1Life <= 251 || vikingFighter2Life <= 251) {
-      		i=9;
-    	}
+    vikingFighter1Life = vikingFighter1Life - vikingFighter2.strength;
+    vikingFighter2Life = vikingFighter2Life - vikingFighter1.strength;
+    if (vikingFighter1Life <= 251 || vikingFighter2Life <= 251) {
+      i=9;
+    }
 	} 
 	console.log (vikingFighter1.name +" tiene "+ vikingFighter1Life + " puntos de vida");
-  	console.log (vikingFighter2.name +" tiene "+ vikingFighter2Life + " puntos de vida");
+  console.log (vikingFighter2.name +" tiene "+ vikingFighter2Life + " puntos de vida");
 }
 
-
+//saxons generator
 
 var SaxonPopulation = function (health, strength){
 	this.health = health;
 	this.strength = strength;
 };
 
-saxon1 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon2 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon3 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon4 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon5 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon6 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon7 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-saxon8 = new SaxonPopulation (getRandomInt(150,500),getRandomInt(50,150));
-
-function finalFight(){
-	var roundNumber = getRandomInt(5,8);
+function saxonGenerator (numSaxon){
+  var saxonArray = [];
+  var saxon={};
+  for (i=0; i<=numSaxon; i++) {
+    saxon[i] = new SaxonPopulation (getRandomInt(150,300),getRandomInt(50,100));
+    saxonArray.push(saxon[i]);
+  }
+  return saxonArray;
 }
+
+
+//the assault
+
+viking1.shout="Ahhhhhh!";
+viking2.shout="al ataquerrrrr!";
+viking3.shout="por valhalla!";
 
 
 

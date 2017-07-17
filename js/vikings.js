1b68ce5698c1f8a89f11f1f9fac2038000303206
koshhi <cesaralvarezmarin@gmail.com>
2017-07-17 16:55:05 +0200

    vikings

--- js/vikings.js
+++ js/vikings.js
@@ -7,3 +7,10 @@ var Population = function (name, health, strength){
 viking1= new Population ("Ragnarok", 1000, 150);
 viking2= new Population ("Asgard", 1500, 250);
 viking3= new Population ("Fernandito", 2000, 100);
+
+var SaxonsPopulation = function (health, strength){
+	this.health = health;
+	this.strength = strength;
+};
+
+saxon1= new SaxonsPopulation (Math
